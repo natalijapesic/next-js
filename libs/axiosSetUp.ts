0bee7c273f34bc5f83c84a39a13bc4ce7f968047
natalija <natalijapesic@elfak.rs>
2022-05-23 16:57:45 +0200

    axiosSetUp

---
+++ libs/axiosSetUp.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosError, AxiosRequestConfig } from "axios";
+// import storeService from "../storeService";
+// import config from "../config/app.config.json";
+
+// axios.defaults.baseURL = `${config.server}`;
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.put["Content-Type"] = "application/json";
+
+axios.interceptors.request.use(
+  async (config: AxiosRequestConfig) => {
+    // let token = storeService.getAccessToken();
+    // if (token) {
+    //   config.headers!["Authorization"] = `Bearer ${token}`;
+    // }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// axios.interceptors.response.use(
+//   (response: AxiosRequestConfig) => {
+//     return response;
+//   },
+//   async (error: AxiosError) => {
+//     if (error.response) {
+//       if (error.response.status === 401) {
+//         error.config.headers!['Authorization'] = `Bearer ${storeService.getAccessToken()}`;
+//         return error.config
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+export default axios;
